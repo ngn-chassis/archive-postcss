77caa53c1289f3b12a48e143e234b074a7cbeb99
Graham Butler <graham@edlio.com>
2017-04-13 20:24:51 -0700

    WIP forms

--- ngn-chassis-detailer/detailer/components.js
+++ ngn-chassis-detailer/detailer/components.js
@@ -1,16 +1,20 @@
-const DetailerButtonComponent = require('./ui-components/button')
-
 class DetailerComponents {
 	constructor (project) {
 		this.project = project
 	}
 
 	get (component) {
-		return this[component]
-	}
+		let definition = this[component]
+		let styles
 
-	get button () {
-		let styles = this.project.utils.parseStylesheet(`${this.project.basePath}/stylesheets/ui-components/button.css`)
+		if (NGN.typeof(definition) === 'array') {
+			styles = this.project.utils.parseStylesheets(definition.map((stylesheet) => {
+				return `${this.project.basePath}${stylesheet}`
+			}))
+
+		} else {
+			styles = this.project.utils.parseStylesheet(`${this.project.basePath}${this[component]}`)
+		}
 
 		styles.walkAtRules((atRule) => {
 			if (atRule.name === 'chassis') {
@@ -23,6 +27,38 @@ class DetailerComponents {
 		})
 
 		return styles
+	}
+
+	get button () {
+		return '/stylesheets/ui-components/button.css'
+	}
+
+	get form () {
+		return [
+			'/stylesheets/ui-components/form/reset.css',
+			'/stylesheets/ui-components/form/standalone-elements.css',
+			'/stylesheets/ui-components/form/layout.css'
+		]
+	}
+
+	get icon () {
+		return '/stylesheets/ui-components/icon.css'
+	}
+
+	get modal () {
+		return '/stylesheets/ui-components/modal.css'
+	}
+
+	get overlay () {
+		return '/stylesheets/ui-components/overlay.css'
+	}
+
+	get tag () {
+		return '/stylesheets/ui-components/tag.css'
+	}
+
+	get table () {
+		return '/stylesheets/ui-components/table.css'
 	}
 }
 
--- ngn-chassis-detailer/detailer/ui-components/button.js
+++
@@ -1,9 +0,0 @@
-const DetailerComponent = require('../component')
-
-class DetailerButtonComponent extends DetailerComponent {
-	constructor (config) {
-		config = NGN.coalesce(config, {})
-
-		super(config)
-	}
-}
