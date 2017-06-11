247ab6d0012f06628264cd9c2d838d5011c49b8f
Graham Butler <graham@grahambutler.com>
2017-06-11 02:37:30 -0700

    WIP mixins

--- ngn-chassis/src/at-rules.js
+++ ngn-chassis/src/at-rules.js
@@ -4,37 +4,13 @@ class ChassisAtRules {
 
 		this.mixins = {
 			'constrain-width': this.constrainWidth.bind(this),
+			'disable-text-selection': this.disableTextSelection.bind(this),
+			'ellipsis': this.ellipsis.bind(this),
+			'ie-only': this.ieOnly.bind(this),
 			'viewport-width': this.viewportWidth.bind(this),
-			'viewport-height': this.viewportHeight.bind(this)
+			'viewport-height': this.viewportHeight.bind(this),
+			'z-index': this.zIndex.bind(this)
 		}
-	}
-
-	viewportWidth () {
-		let { layout, settings, utils } = this.chassis
-
-		let operator = this.cfg.args[0]
-		let width = parseInt(this.cfg.args[1])
-
-		let mediaQuery = utils.newMediaQuery(
-			layout.getMediaQueryParams('width', operator, width),
-			this.nodes
-		)
-
-		this.atRule.replaceWith(mediaQuery)
-	}
-
-	viewportHeight () {
-		let { layout, settings, utils } = this.chassis
-
-		let operator = this.cfg.args[0]
-		let height = parseInt(this.cfg.args[1])
-
-		let mediaQuery = utils.newMediaQuery(
-			layout.getMediaQueryParams('height', operator, height),
-			this.nodes
-		)
-
-		this.atRule.replaceWith(mediaQuery)
 	}
 
 	/**
@@ -87,6 +63,97 @@ class ChassisAtRules {
 
 		this.atRule.replaceWith(decls)
 	}
+
+	/**
+	 * @mixin disableTextSelection
+	 * Disable users' ability to select a text node
+	 */
+	disableTextSelection () {
+		let { utils } = this.chassis
+
+		this.atRule.replaceWith([
+      utils.newDecl('-webkit-touch-callout', 'none'),
+      utils.newDecl('-webkit-user-select', 'none'),
+      utils.newDecl('-khtml-user-select', 'none'),
+			utils.newDecl('-moz-user-select', 'none'),
+      utils.newDecl('-ms-user-select', 'none'),
+      utils.newDecl('-o-user-select', 'none'),
+			utils.newDecl('user-select', 'none')
+    ])
+	}
+
+	/**
+	 * @mixin ellipsis
+	 */
+  ellipsis () {
+		let { utils } = this.chassis
+
+		this.atRule.replaceWith([
+      utils.newDecl('white-space', 'nowrap'),
+      utils.newDecl('overflow', 'hidden'),
+      utils.newDecl('text-overflow', 'ellipsis')
+    ])
+  }
+
+	/**
+   * @mixin ieOnly
+   * TODO: Implement version support
+   */
+  ieOnly () {
+		let { utils } = this.chassis
+
+		this.atRule.replaceWith(utils.newAtRule({
+      name: 'media',
+      params: 'all and (-ms-high-contrast: none)',
+      nodes: this.nodes.map((rule) => {
+        rule.selector = `*::-ms-backdrop, ${rule.selector}`
+        return rule
+      })
+    }))
+  }
+
+	viewportWidth () {
+		let { layout, settings, utils } = this.chassis
+
+		let operator = this.cfg.args[0]
+		let width = parseInt(this.cfg.args[1])
+
+		let mediaQuery = utils.newMediaQuery(
+			layout.getMediaQueryParams('width', operator, width),
+			this.nodes
+		)
+
+		this.atRule.replaceWith(mediaQuery)
+	}
+
+	viewportHeight () {
+		let { layout, settings, utils } = this.chassis
+
+		let operator = this.cfg.args[0]
+		let height = parseInt(this.cfg.args[1])
+
+		let mediaQuery = utils.newMediaQuery(
+			layout.getMediaQueryParams('height', operator, height),
+			this.nodes
+		)
+
+		this.atRule.replaceWith(mediaQuery)
+	}
+
+	/**
+   * @mixin zIndex
+   * Get calculated z-index value from project settings
+   */
+  zIndex () {
+		let { settings, utils } = this.chassis
+    let index = settings.zIndex[this.cfg.args[0]]
+
+    if (!index) {
+      console.error(`[ERROR] Chassis z-index: Invalid identifier. Accepted values: ${Object.keys(settings.zIndex).join(', ')}`)
+    }
+
+    this.atRule.replaceWith(utils.newDecl('z-index', index))
+  }
 
 	process (root, atRule, data) {
 		this.root = root
