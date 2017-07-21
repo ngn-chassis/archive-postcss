d66a2fac5063ccf37ac54d2ff9bffa6755503b97
Graham Butler <graham@edlio.com>
2017-07-20 20:08:41 -0700

    tag tweaks

--- ngn-chassis/src/components/tag/component.js
+++ ngn-chassis/src/components/tag/component.js
@@ -13,26 +13,30 @@ class ChassisTagComponent extends ChassisComponent {
 	
 	get variables () {
 		let { settings, typography, utils } = this.chassis
-    let { fontSize, lineHeight } = settings.typography.ranges.first.typography.small
+    let { fontSize, lineHeight } = this.baseTypography.small
 
     let lineHeightMultiplier = utils.units.toEms(lineHeight, fontSize)
     let inlineHeight = typography.calculateInlineHeight(lineHeightMultiplier)
-		let padding = (inlineHeight - lineHeightMultiplier) / 2
+		let paddingRoot = Math.log(lineHeightMultiplier) / 2
 		
-		let iconDimension = `${lineHeightMultiplier - (settings.typography.scaleRatio - 1)}em`
-		let iconOffset = `-${(Math.log(lineHeightMultiplier) / 2) - utils.units.toEms(fontSize / (settings.typography.scaleRatio * 10), fontSize)}em`
+		let iconDimension = `${lineHeightMultiplier / settings.typography.scaleRatio}em`
+		let iconOffset = `-${paddingRoot - utils.units.toEms(fontSize / (settings.typography.scaleRatio * 10), fontSize)}em`
 		
 		if (iconOffset < 0) {
 			iconOffset = 0
 		}
+		
+		let paddingX = settings.typography.scaleRatio - 1
+		let paddingY = paddingRoot * (settings.typography.scaleRatio - 1)
 
     return {
 			'font-size': `${utils.units.toEms(fontSize, this.baseTypography.root.fontSize)}em`,
       'line-height': `${lineHeightMultiplier}`,
+			'padding': `${paddingY}em ${paddingX}em`,
 			'icon-width': iconDimension,
 			'icon-height': iconDimension,
 			'icon-offset': `translateX(${iconOffset})`,
-			'pill-padding-x': `${Math.sin(lineHeightMultiplier) / 2}em`,
+			'pill-padding-x': `${Math.sin(lineHeightMultiplier) / settings.typography.scaleRatio}em`,
 			'pill-border-radius': `${lineHeightMultiplier}em`
     }
 	}
